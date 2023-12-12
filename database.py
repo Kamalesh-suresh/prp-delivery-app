a413a0afa875cbb690ec49658a047654d39120d5
kamalesh.suresh <kamalesh.suresh@psiog.com>
2023-12-12 20:47:55 +0530

    Added config file

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
-import motor
+from config import MONGODB_CONNECTION_STRING
 import motor.motor_asyncio
 from models import User
 from beanie import init_beanie
 
-
 async def init_db():
-    client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://kamalesh:kamal1234@cluster0.y53uz6w.mongodb.net/?retryWrites=true&w=majority")
-    print(client,"ssssssssssssssssssssssssss")
+    connection_string = MONGODB_CONNECTION_STRING
+    if not connection_string:
+        raise ValueError("MONGODB_CONNECTION_STRING environment variable is not set")
+
+    client = motor.motor_asyncio.AsyncIOMotorClient(connection_string)
     await init_beanie(database=client["foody"], document_models=[User])
