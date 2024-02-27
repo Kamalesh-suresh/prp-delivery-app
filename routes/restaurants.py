cfe9401f425c900128267db59e592b8c137f69f6
kamalesh.suresh <kamalesh.suresh@psiog.com>
2024-02-27 21:30:24 +0530

    Added aggregation pipeline to get all restaurants

--- routes/restaurants.py
+++ routes/restaurants.py
@@ -5,7 +5,7 @@
 from typing import List, Union
 
 from beanie import PydanticObjectId
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 
 from schema_model.menus import Menu
 from schema_model.restaurants import Restaurant
@@ -14,14 +14,74 @@ restaurants_router = APIRouter()
 
 
 @restaurants_router.get("/")
-async def get_allrestaurants():
-    restaurants = await Restaurant.find_all().to_list()
-    for restaurant in restaurants:
-        menu_id = restaurant.menu_id
-        menu = await Menu.find_one(Menu.id == menu_id)
-        if menu:
-            restaurant.menu = menu
-    return restaurants
+async def get_all_restaurants(
+    count: int = Query(10, ge=1),
+    page: int = Query(1, ge=1),
+    search: str = Query(None),
+    sort: str = Query("desc", regex=r"^(asc|desc)$"),
+):
+    skip = (page - 1) * count
+
+    sort_stage = {"title": 1 if sort == "asc" else -1}
+
+    pipeline = [
+        {"$sort": sort_stage},
+        {"$skip": skip},
+        {"$limit": count},
+        {
+            "$lookup": {
+                "from": "menus",
+                "localField": "menu_id",
+                "foreignField": "_id",
+                "as": "menu",
+            }
+        },
+        {"$unwind": "$menu"},
+        {
+            "$project": {
+                "_id": {"$toString": "$_id"},
+                "title": 1,
+                "description": 1,
+                "menu": {
+                    "cusine": 1,
+                    "starters": 1,
+                    "drinks": 1,
+                    "breads": 1,
+                    "main_course": 1,
+                },
+            }
+        },
+    ]
+
+    if search:
+        pipeline.insert(
+            0,
+            {
+                "$search": {
+                    "index": "default",
+                    "autocomplete": {"query": search, "path": "title"},
+                }
+            },
+        )
+
+    menu_pipeline = [
+        {
+            "$lookup": {
+                "from": "menus",
+                "localField": "menu_id",
+                "foreignField": "_id",
+                "as": "menu",
+            },
+        },
+        {"$unwind": "$menu"},
+    ]
+
+    total_count = await Restaurant.count()
+    menu_lookup = await Restaurant.aggregate(menu_pipeline).to_list()
+    print(menu_lookup)
+    restaurants_with_menus = await Restaurant.aggregate(pipeline).to_list()
+
+    return {"total_count": total_count, "restaurants": restaurants_with_menus}
 
 
 @restaurants_router.get("/{restaurant_id}")
